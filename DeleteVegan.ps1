cc4107ab46e7d72004e509e70cbc6811b0a89522
alienemperor <32971607+alienemperor@users.noreply.github.com>
2018-02-04 10:59:43 -0700

    Add files via upload

---
+++ DeleteVegan.ps1
@@ -0,0 +1,4 @@
+﻿$username = Read-Host "what is the name of the user you want to remove?"
+Remove-WebVirtualDirectory -Name . -Site VEGAN -Application $username
+Remove-Item -Path "C:\inetpub\ftproot\LocalUser\$username" -Recurse
+Remove-LocalUser -Name $username
