d591018853aaf28ce0c2ce851a4dd206ade1aa74
alienemperor <32971607+alienemperor@users.noreply.github.com>
2018-02-13 11:47:43 -0700

    Add files via upload

---
+++ Maintenance/NewHardDiskVegan.ps1
@@ -0,0 +1,8 @@
+﻿#$VMName = Read-Host 'Name of new VM:' 
+New-VM -Name Vegan-Production -Generation 2 -SwitchName External -Path C:\Vms\Vegan-Production\
+
+Set-VM -Name Vegan-Production -DynamicMemory -MemoryMinimumBytes 1GB -MemoryMaximumBytes 6GB -MemoryStartupBytes 2GB -ProcessorCount 4
+
+copy C:\VMs\Sysprep\SVR-2012.vhdx C:\VMs\Vegan-Production\Vegan-Production_Disk1.vhdx
+
+Add-VMHardDiskDrive -VMName Vegan-Production -Path C:\VMs\Vegan-Production\Vegan-Production_Disk1.vhdx
